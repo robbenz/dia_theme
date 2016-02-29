c86def73101307fea866986c1411548dd971b079
Rob Benz <benz_rob@yahoo.com>
2016-02-29 14:34:57 -0500

    css for accordian

--- category.php
+++ category.php
@@ -2,7 +2,11 @@
 
 <div class="container">
   <div class="row">
-    
+
+    <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
+       <?php get_template_part('includes/sidebar'); ?>
+    </div>
+
     <div class="col-xs-12 col-sm-8">
       <div id="content" role="main">
         <h1>Category: <?php echo single_cat_title(); ?></h1>
@@ -10,11 +14,9 @@
         <?php get_template_part('includes/loops/content', get_post_format()); ?>
       </div><!-- /#content -->
     </div>
-    
-    <div class="col-xs-6 col-sm-4" id="sidebar" role="navigation">
-       <?php get_template_part('includes/sidebar'); ?>
-    </div>
-    
+
+
+
   </div><!-- /.row -->
 </div><!-- /.container -->
 
