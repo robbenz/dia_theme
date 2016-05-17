6b0ce07ea19e086178f13a4a9c6ac735be5a47a1
Rob Benz <benz_rob@yahoo.com>
2016-05-17 10:19:10 -0400

    pishing curtain form

--- includes/curtian-form.php
+++ includes/curtian-form.php
@@ -1,9 +1,12 @@
+<div style="90%;">
+
 <h2>Privacy Curtain Sizing Form</h2>
 <?php
 
 
  if ( function_exists( 'ccf_output_form' ) ) {
-    ccf_output_form( '58449' );
+    ccf_output_form( '59858' );
 }
 
 ?>
+</div>
