eca1ad77155d09d39103b9baf6f39027ef1fdb37
Rob Benz <benz_rob@yahoo.com>
2016-09-27 15:06:55 -0400

    added loop through category based on slug - and circle img

--- includes/cat_loops/beds_pm.php
+++ includes/cat_loops/beds_pm.php
@@ -1,17 +1,25 @@
 <div class="header-wrap-text-medical-equipment">
   <h2 class="header-wrap-text-medical-equipment-header">Hill-Rom Bed Repairs</h2>
-    Add to your EMS division or Cardiac lab with our most reliable AEDs, AED trainers, defibrillators, and arrhythmia simulators. Becoming familiar with these products and their functions is crucial to a student’s training. They are easy to program and operate, and allow students to practice how to react in different life-threatening, cardiac scenarios. We sell new and reconditioned Zoll, Phillips, and Physio-Control units, which all offer industry-leading technology. We promise each device to be long-lasting and reliable!
-  </div>
+Preventive Maintenance Services from DiaMedical USA will evaluate the condition of your equipment and determine the most cost-effective and manageable solution to ensure its overall performance, safety and reliability.  </div>
 <ul class="products">
 
     <?php
-        $args = array( 'post_type' => 'product', 'posts_per_page' => 12, 'product_cat' => 'hill-rom-beds', 'orderby' => 'title' );
+        $args = array( 'post_type' => 'product', 'posts_per_page' => 12, 'product_cat' => 'hill-rom-beds', 'orderby' => 'title', 'order'   => 'ASC' );
         $loop = new WP_Query( $args );
         while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 
                 <li class="product">
-                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
-                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+                    <a href="<?php echo esc_url(add_query_arg('vfb-field-97', 'hello', get_permalink(62913) ) ); ?>">
+                        <?php
+                        if (has_post_thumbnail( $loop->post->ID )) {
+                           echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
+                           echo '<img style="width: 5.6vw; height:5.6vw; max-height:80px; max-width:80px; position:absolute; bottom: 50px; right:6px; z-index:99" src="';
+                           echo site_url();
+                           echo '/wp-content/imgs/repairs-preventive-maintenance.png" />';
+                         } else {
+                         echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />';
+                       }
+                         ?>
                         <h3><?php the_title(); ?></h3>
                     </a>
                 </li>
