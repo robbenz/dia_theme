6b54bd3ce525309f65ed2505d0a55e5e7f7dc42e
Rob Benz <benz_rob@yahoo.com>
2016-09-27 12:30:51 -0400

    grab category by slug - and loop through products

--- includes/cat_loops/beds_pm.php
+++ includes/cat_loops/beds_pm.php
@@ -4,11 +4,18 @@
   </div>
 <ul class="products">
 
-  <li class="product-category product first">
-    <a href="<?php echo add_query_arg( 'vfb-field-89', 'Does This Work?', get_permalink(62913) );?>">
-      <img src="<?php echo site_url(); ?>/wp-content/uploads/2015/07/Hill-Rom-835_400x400-258x275.png" alt="Batteries, Cables &amp; Sensors" width="250" height="275" />
-      <h3>Hill-Rom 835</h3>
-    </a>
-  </li>
+    <?php
+        $args = array( 'post_type' => 'product', 'posts_per_page' => 12, 'product_cat' => 'hill-rom-beds', 'orderby' => 'title' );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 
-</ul>
+                <li class="product">
+                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+                        <h3><?php the_title(); ?></h3>
+                    </a>
+                </li>
+
+    <?php endwhile; ?>
+    <?php wp_reset_query(); ?>
+</ul><!--/.products-->
