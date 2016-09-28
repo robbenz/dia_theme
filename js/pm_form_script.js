9e1caaabc26d6d14eba97386b7fab82c3270d4f9
Rob Benz <benz_rob@yahoo.com>
2016-09-28 09:21:25 -0400

    cleaned up output remove '-' and change to Title Case

--- js/pm_form_script.js
+++ js/pm_form_script.js
@@ -1,9 +1,10 @@
 $(window).load(function() {
   $mySpot = $(location).attr('search');
   $mySlug = $mySpot.replace('?','');
+  $myCleanSlug = $mySlug.replace(/\-/g, ' ');
 
   $(function () {
-    $('#vfb-field-97').val($mySlug);
+    $('#vfb-field-97').val($myCleanSlug).css('text-transform','capitalize');
   });
 
 });
