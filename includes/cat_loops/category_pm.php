3df1d650569c6e7b32fb744b5ddba37048c04226
Rob Benz <benz_rob@yahoo.com>
2016-09-29 14:48:42 -0400

    cleaned up new category page - so more like product-level template page

--- includes/cat_loops/category_pm.php
+++ includes/cat_loops/category_pm.php
@@ -7,39 +7,27 @@
 
 <ul class="products">
 
-
 <?php
 
-// 6398
-// woocommerce_subcats_from_parentcat_by_ID(6398);
-
-
 $args = array( 'hierarchical' => 1, 'show_option_none' => '', 'hide_empty' => 0, 'parent' => '6398', 'taxonomy' => 'product_cat' );
-
 $subcats = get_categories($args);
+foreach ($subcats as $sc) :
+//  $link = get_term_link( $sc->slug, $sc->taxonomy );
+  $thumbnail_id = get_woocommerce_term_meta( $sc->term_id, 'thumbnail_id', true );
+  $image = wp_get_attachment_url( $thumbnail_id );
+  ?>
+  <li class ="product-category product">
+    <a href=" ">
+      <img src="<?php echo $image ?>" />
+      <?php
+      echo '<img style="width: 5.6vw; height:5.6vw; max-height:80px; max-width:80px; position:absolute; bottom: 50px; right:6px; z-index:99" src="';
+      echo site_url();
+      echo '/wp-content/imgs/repairs-preventive-maintenance.png" />';
+      ?>
+      <h3><?php echo $sc->name ?></h3>
+    </a>
+  </li>
 
-foreach ($subcats as $sc) {
-
-    $link = get_term_link( $sc->slug, $sc->taxonomy );
-
-    $thumbnail_id = get_woocommerce_term_meta( $sc->term_id, 'thumbnail_id', true );
-    $image = wp_get_attachment_url( $thumbnail_id );
-
-
-echo '<li class ="product-category product">';
-echo '<a href="'. $link .'">';
-
-echo '<img src="' . $image .'" />' ;
-echo '<img style="width: 5.6vw; height:5.6vw; max-height:80px; max-width:80px; position:absolute; bottom: 50px; right:6px; z-index:99" src="';
-echo site_url();
-echo '/wp-content/imgs/repairs-preventive-maintenance.png" />';
-echo '<h3>'.$sc->name.'</h3>';
-echo '</a>';
-echo '</li>';
-  }
-
-
-
-?>
+<?php endforeach; ?>
 
 </ul><!--/.products-->
