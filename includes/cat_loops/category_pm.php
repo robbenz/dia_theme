f22455a90f87a3fa152093a48e1fb80bbffdaa5c
Rob Benz <benz_rob@yahoo.com>
2016-09-29 14:41:46 -0400

    working gallery page - loop through - not linking to corrent repairs products pages yet

--- includes/cat_loops/category_pm.php
+++ includes/cat_loops/category_pm.php
@@ -6,38 +6,40 @@
 </div>
 
 <ul class="products">
-  <?php
-    $counter = 0;
-    $args = array( 'post_type' => 'product_cat', 'posts_per_page' => 36, 'product_cat' => 'hospital-beds', 'orderby' => 'title', 'order'   => 'ASC' );
-    $loop = new WP_Query( $args );
-    while ( $loop->have_posts() ) : $loop->the_post(); global $product;
-    $pm_product_id = $loop->post->ID;
-    $product = get_post( $pm_product_id );
-    $slug = $product->post_name;
-    if ($counter % 3 == 0) {
-      $first = ' first' ;
-    } elseif ($counter % 3 != 0) {
-      $first = '' ;
-    }  ?>
-    <li class="product<?php echo $first; ?>">
-      <a href=" ">
-        <?php
-        if (has_post_thumbnail( $loop->post->ID )) {
-          echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
-          echo '<img style="width: 5.6vw; height:5.6vw; max-height:80px; max-width:80px; position:absolute; bottom: 50px; right:6px; z-index:99" src="';
-          echo site_url();
-          echo '/wp-content/imgs/repairs-preventive-maintenance.png" />';
-        } else {
-          echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />';
-        }
-        ?>
-        <h3><?php the_title(); ?></h3>
-      </a>
-    </li>
-    <?php
-    $counter++ ;
-  endwhile;
-  wp_reset_query();
-  ?>
+
+
+<?php
+
+// 6398
+// woocommerce_subcats_from_parentcat_by_ID(6398);
+
+
+$args = array( 'hierarchical' => 1, 'show_option_none' => '', 'hide_empty' => 0, 'parent' => '6398', 'taxonomy' => 'product_cat' );
+
+$subcats = get_categories($args);
+
+foreach ($subcats as $sc) {
+
+    $link = get_term_link( $sc->slug, $sc->taxonomy );
+
+    $thumbnail_id = get_woocommerce_term_meta( $sc->term_id, 'thumbnail_id', true );
+    $image = wp_get_attachment_url( $thumbnail_id );
+
+
+echo '<li class ="product-category product">';
+echo '<a href="'. $link .'">';
+
+echo '<img src="' . $image .'" />' ;
+echo '<img style="width: 5.6vw; height:5.6vw; max-height:80px; max-width:80px; position:absolute; bottom: 50px; right:6px; z-index:99" src="';
+echo site_url();
+echo '/wp-content/imgs/repairs-preventive-maintenance.png" />';
+echo '<h3>'.$sc->name.'</h3>';
+echo '</a>';
+echo '</li>';
+  }
+
+
+
+?>
 
 </ul><!--/.products-->
