b94937001fd9f3d88279835cb982013b2ca071b0
Rob Benz <benz_rob@yahoo.com>
2016-09-29 08:24:49 -0400

    hello

--- js/pm_form_script.js
+++ js/pm_form_script.js
@@ -1,10 +1,14 @@
 $(window).load(function() {
-  $mySpot = $(location).attr('search');
-  $mySlug = $mySpot.replace('?','');
-  $myCleanSlug = $mySlug.replace(/\-/g, ' ');
+    $mySpot = $(location).attr('search');
+    $mySlug = $mySpot.replace('?', '');
+    $myCleanSlug = $mySlug.replace(/\-/g, ' ');
 
-  $(function () {
-    $('#vfb-field-97').val($myCleanSlug).css('text-transform','capitalize');
-  });
+    $(function() {
+        $('#vfb-field-97').val($myCleanSlug).css('text-transform', 'capitalize');
+    });
 
 });
+
+$("label[for='vfb-field-103']").hide();
+$("label[for='vfb-field-105']").hide();
+$("label[for='vfb-field-106']").hide();
