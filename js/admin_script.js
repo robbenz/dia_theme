b5d6b49028a1f1d298e37795300119fcb63507c4
Rob Benz <benz_rob@yahoo.com>
2016-10-05 11:23:12 -0400

    work on js admin ;

--- js/admin_script.js
+++ js/admin_script.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function() {
-  jQuery("#benz_condition_select").hide();
     jQuery("#benz_product_select").find('select').change(function() {
         var $val = jQuery(this).val();
-
-        if($val != 'Part'){
-            jQuery('#benz_condition_select').hide(200);
-        }else{
-            jQuery('#benz_condition_select').show(300);
+        if($val == 'Part'){
+          jQuery('#benz_condition_select').show();
+        } else if($val != 'Part'){
+          jQuery('#benz_condition_select').hide(200);
+        } else {
+          jQuery('#benz_condition_select').show(300);
         }
     });
 });
