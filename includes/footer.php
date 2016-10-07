f6427c9cc2b579302cd46db6b8e280a660cf70ab
09jdphil <jphillips@diamedical.com>
2016-10-07 16:27:43 -0400

    footer work

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,71 @@
-<footer id="colophon" class="site-footer container-fluid" role="contentinfo">
+<footer id="colophon" class="site-footer" role="contentinfo">
   <div id="footer2014">
-  </div>
+  <div id="footerwrap" class="container">
+  <div id="footerlogos" class="row">
+  <div id="diamedicallogo" class="col-xs-4"><a href="<?php echo site_url(); ?>"><img src="https://diamedicalusa.com/wp-content/imgs/footer-sample/diamedicallogosmall.png" /></a></div>
+  <div id="medmatlogo" class="col-xs-4"><a href="http://medmattress.com"><img src="https://diamedicalusa.com/wp-content/imgs/footer-sample/medmattlogo.png" /></a></div>
+  <div id="simlablogo" class="col-xs-4"><a href="<?php echo site_url(); ?>/simlabsolutions/"><img src="<?php echo site_url(); ?>/wp-content/imgs/footer-sample/simlablogo.png" /></a></div>
+  </div><!--footerlogos-->
+
+  <div id="footerconnect">
+  <span class="connecttop">p. (877) 593-6011<br />f.&nbsp;&nbsp;(248) 671-1550<br /></span>
+  <span class="connectbottom">Info@DiaMedicalUSA.com<br />5807 W. Maple, Suite #175, West Bloomfield, MI 48322<br /></span>
+  <br />
+
+  <span class="socialicons2014">
+  <a href="http://www.facebook.com/pages/DiaMedical-USA/231317596883279" target="_blank"><img style="padding:0px 10px 0px 0px;" src="https://diamedicalusa.com/wp-content/imgs/social/facebook-hover.png"
+  onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/facebook.png';"
+  onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/facebook-hover.png';"/></a>
+
+  <a href="http://twitter.com/DiaMedicalUSA" target="_blank"><img style="padding:0px 15px 0px 15px;" src="https://diamedicalusa.com/wp-content/imgs/social/twitter-hover.png"
+  onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/twitter.png';"
+  onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/twitter-hover.png';"/></a>
+
+  <a href="http://www.linkedin.com/company/2439975?goback=.fcs_GLHD_diamedical+usa_false_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2&trk=ncsrch_hits" target="_blank"><img style="padding:0px 15px 0px 15px;" src="https://diamedicalusa.com/wp-content/imgs/social/linkedin-hover.png"
+  onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/linkedin.png';"
+  onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/linkedin-hover.png';"/></a>
+
+  <a href="http://www.pinterest.com/medmattress/" target="_blank"><img style="padding:0px 15px 0px 15px;" src="https://diamedicalusa.com/wp-content/imgs/social/pinterest-hover.png"
+  onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/pinterest.png';"
+  onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/pinterest-hover.png';"/></a>
+
+  <a href="http://diamedicalusa.com/blog/" target="_blank"><img style="padding:0px 0px 0px 15px;" src="https://diamedicalusa.com/wp-content/imgs/social/wordpress-hover.png"
+  onMouseOver="this.src='https://diamedicalusa.com/wp-content/imgs/social/wordpress.png';"
+  onMouseOut="this.src='https://diamedicalusa.com/wp-content/imgs/social/wordpress-hover.png';"/></a>
+  </span><!--socialicons2014-->
+
+</div><!--footerconnect-->
+
+<div id="footerlinks1">
+  <a href="<?php echo site_url(); ?>">Home</a><br />
+  <a href="<?php echo site_url(); ?>/medical-equipment/">Products</a><br />
+  <a href="<?php echo site_url(); ?>/results/keyword/search+for+parts/search-in/product/cat-in/all/search-other/productPeepOpen_skuPeepOpen_cat">Manufacturers</a><br />
+  <a href="<?php echo site_url(); ?>/results/keyword/search+for+parts/search-in/product/cat-in/all/search-other/productPeepOpen_skuPeepOpen_cat">Parts</a><br />
+  <a href="#" class="eModal-2">Contact</a><br />
+<a href="<?php echo site_url(); ?>/blog">Blog</a><br />
+</div><!--footerlinks1-->
+
+<div id="footerlinks2">
+  <p class="popularproducts">Popular Products</p><br />
+  <a href="<?php echo site_url(); ?>/product-category/hospital-beds/">Hospital Beds</a><br />
+  <a href="http://medmattress.com/product-category/mattresses/stretcher-gurney/">Stretcher Mattresses</a><br />
+  <a href="<?php echo site_url(); ?>/product-category/hill-rom-parts-online/">Replacement Parts</a><br />
+  <a href="<?php echo site_url(); ?>/product-category/carts-storage/loaded-crash-carts">Loaded Crash Carts&trade;</a><br />
+  <a href="<?php echo site_url(); ?>/product-category/headwalls/">Headwall Units</a><br />
+  <a href="<?php echo site_url(); ?>/product-category/simulated-iv-bags/">IV Fluids</a><br />
+</div><!--footerlinks2-->
+
+<div id="footerlinks3">
+<p class="popularproducts">&nbsp;&nbsp;</p><br />
+<a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/infusion-pumps">Infusion Pumps</a><br />
+<a href="<?php echo site_url(); ?>/results/keyword/caster/search-in/product/cat-in/all/search-other/product,p_sku,p_cat">Casters &amp; Wheels</a><br />
+<a href="http://medmattress.com/product-category/mattresses/operating-table/">OR Table Pads</a><br />
+<a href="http://medmattress.com/product-category/mattresses/">Mattresses</a><br />
+<a href="<?php echo site_url(); ?>/site-repairs-preventive-maintenance/">Repairs</a><br />
+<a href="<?php echo site_url(); ?>/product-category/furniture/">Furnishings</a><br />
+</div><!--footerlinks3-->
+</div>
+  </div><!--footer2014-->
 </footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
