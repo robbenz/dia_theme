bd726c0a3cf535f7dea652d406902029267775d6
Rob Benz <benz_rob@yahoo.com>
2016-10-12 15:12:18 -0400

    workflow on PM page

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../css/ristrettopro_regular_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+		<link rel="stylesheet" href="../css/ristrettopro_medium_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+		<link rel="stylesheet" href="../css/ristrettopro_light_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+		<link rel="stylesheet" href="../css/ristrettopro_bold_macroman/stylesheet.css" type="text/css" charset="utf-8" />
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800italic,800' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 
