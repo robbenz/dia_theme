12aa620d4c80b2df1d8d164e8d9083a1b38bb95a
09jdphil <jphillips@diamedicalusa.com>
2016-11-08 16:57:11 -0500

    checkout page

--- js/bst.js
+++ js/bst.js
@@ -1,57 +1,59 @@
-(function ($) {
+(function($) {
 
-	"use strict";
+    "use strict";
 
-	$(document).ready(function() {
+    $(document).ready(function() {
 
-		// Comments
-		$(".commentlist li").addClass("panel panel-default");
-		$(".comment-reply-link").addClass("btn btn-default");
+        // Comments
+        $(".commentlist li").addClass("panel panel-default");
+        $(".comment-reply-link").addClass("btn btn-default");
 
-		// Forms
-		$('select, input[type=text], input[type=email], input[type=password], textarea').addClass('form-control');
-		$('input[type=submit]').addClass('btn btn-primary');
+        // Forms
+        $('select, input[type=text], input[type=email], input[type=password], textarea').addClass('form-control');
+        $('input[type=submit]').addClass('btn btn-primary');
 
-		// You can put your own code in here
+        // You can put your own code in here
 
-		/*** Add classes to #PM # Shipping -- product page on load & Resize ***/
-		function addClassesProducts($when) {
-			$(window).on($when, function() {
-				if($(window).width() < 992) {
-						$('#ship-repair-container').addClass('container');
-						$('#ship-row').addClass('row');
-						$('repair-row').addClass('row');
-				} else {
-					$('#ship-repair-container').removeClass('container');
-					$('#ship-row').removeClass('row');
-					$('repair-row').removeClass('row');
-				}
-			})
-		}
-		addClassesProducts('load');
-		addClassesProducts('resize');
-		/*** END ***/
+        /*** Add classes to #PM # Shipping -- product page on load & Resize ***/
+        function addClassesProducts($when) {
+            $(window).on($when, function() {
+                if ($(window).width() < 992) {
+                    $('#ship-repair-container').addClass('container');
+                    $('#ship-row').addClass('row');
+                    $('repair-row').addClass('row');
+                } else {
+                    $('#ship-repair-container').removeClass('container');
+                    $('#ship-row').removeClass('row');
+                    $('repair-row').removeClass('row');
+                }
+            })
+        }
+        addClassesProducts('load');
+        addClassesProducts('resize');
+        /*** END ***/
 
-		/*** Add classes to #mobile-navbar on load & Resize ***/
-		function addClassesMobile($when) {
-			$(window).on($when, function() {
-				if($(window).width() < 992) {
-						$('#mobile-navbar').addClass('navbar navbar-default navbar-fixed-top');
-						$('#form-row').addClass('row');
-						$('#drop-menu').removeClass('navbar navbar-default navbar-static-top');
-				} else {
-						$('#mobile-navbar').removeClass('navbar navbar-default navbar-fixed-top');
-						$('#form-row').removeClass('row');
-						$('#drop-menu').addClass('navbar navbar-default navbar-static-top');
-				}
-			})
-		}
-		addClassesMobile('load');
-		addClassesMobile('resize');
-		/*** END ***/
+        /*** Add classes to #mobile-navbar on load & Resize ***/
+        function addClassesMobile($when) {
+            $(window).on($when, function() {
+                if ($(window).width() < 992) {
+                    $('#mobile-navbar').addClass('navbar navbar-default navbar-fixed-top');
+                    $('#form-row').addClass('row');
+                    $('#drop-menu').removeClass('navbar navbar-default navbar-static-top');
+                } else {
+                    $('#mobile-navbar').removeClass('navbar navbar-default navbar-fixed-top');
+                    $('#form-row').removeClass('row');
+                    $('#drop-menu').addClass('navbar navbar-default navbar-static-top');
+                }
+            })
+        }
+        addClassesMobile('load');
+        addClassesMobile('resize');
+        /*** END ***/
 
+        $("[href='#previous']").click(function() {
+            $(".current").next().removeClass("done");
+        });
 
-
-	});
+    });
 
 }(jQuery));
