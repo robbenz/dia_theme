bbc9ddd6cc061d401a079f6aa7877dff0098f53a
Rob Benz <benz_rob@yahoo.com>
2016-12-05 07:54:23 -0500

    sls oh fuck

--- includes/sim-lab.php
+++ includes/sim-lab.php
@@ -1,14 +1,14 @@
 <div class="container">
-  <div class="row"> 
+  <div class="row">
     <img src="<?php echo site_url();?>/wp-content/imgs/includes/sim-lab.png" />
-    <span class="sls_header">exclusive Healthcare Education</span><br />
-    <span class="sls_copy">that Improve Training Simulations for Students &amp; Instructors</span>
+    <span style="font-family: 'ristretto_probold', Arial, sans-serif; letter-spacing:2.5px; color:#3e87cb; font-size:56px;  margin-right: 2.45em; float:right; text-transform:uppercase; line-height: 38px;">exclusive Healthcare Education</span><br />
+    <span style="color: #3e87cb; margin-top: -52px; float: right; font-family: 'ristretto_proregular',Arial,sans-serif;font-size: 35px; letter-spacing: 1.5px; margin-right: 2.4em;">that Improve Training Simulations for Students &amp; Instructors</span>
   </div>
 </div>
 </div><!-- #content-->
 </div><!--.row -->
 </div><!--.container -->
-<div id="sls_red_div">ALL SIMLABSOLUTIONS PRODUCTS ARE FOR INSTRUCTIONAL USE ONLY. NOT FOR HUMAN OR ANIMAL INJECTION OR INGESTION.</div>
+<div style="width:100%; text-align:center; background-color:#d6001c; padding:4px 0; color:#fff; font-size: 17px; letter-spacing: 1px;">ALL SIMLABSOLUTIONS PRODUCTS ARE FOR INSTRUCTIONAL USE ONLY. NOT FOR HUMAN OR ANIMAL INJECTION OR INGESTION.</div>
 <div class="container woocommerce">
 <div class="row">
     <div class="col-xs-6" id="sidebar" role="navigation">
@@ -28,7 +28,7 @@
         </div>
         <div class="col-md-6">
         	<a href="<?php echo site_url(); ?>/product-category/emergency-rescue/loaded-emergency-packs/">
-        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-Square.png" 
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-Square.png"
         		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-hover.png';"
     				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-Square.png';" />
         	</a>
@@ -37,21 +37,21 @@
       <div class="row" style="margin-bottom:14px;">
         <div class="col-md-4">
         	<a href="<?php echo site_url(); ?>/product-category/simulated-iv-bags/">
-        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square.png"  
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square.png"
         		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square-Hover.png';"
     				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square.png';" />
         	</a>
         </div>
         <div class="col-md-4">
         	<a href="<?php echo site_url(); ?>/product-category/simulated-iv-bags/">
-        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square.png"  
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square.png"
         		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square-Hover.png';"
     				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square.png';" />
         	</a>
         </div>
         <div class="col-md-4">
         	<a href="<?php echo site_url(); ?>/product-category/supply-kits/">
-        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square.png"  
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square.png"
         		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square-Hover.png';"
     				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square.png';" />
         	</a>
