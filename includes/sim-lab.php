154d75022c9434c07fe8d053bea0ae5c9fc47929
Benzy25 <benz_rob@yahoo.com>
2016-12-04 19:42:37 -0500

    ems and sls page and dia home and ems packs, and everything done

--- includes/sim-lab.php
+++ includes/sim-lab.php
@@ -1,42 +1,64 @@
 <div class="container">
-<div class="row"> 
-<img src="<?php echo site_url();?>/wp-content/imgs/includes/sim-lab.png" />
-<span class="sls_header">exclusive Healthcare Education</span><br />
-<span class="sls_copy">that Improve Training Simulations for Students & Instructors</span>
+  <div class="row"> 
+    <img src="<?php echo site_url();?>/wp-content/imgs/includes/sim-lab.png" />
+    <span class="sls_header">exclusive Healthcare Education</span><br />
+    <span class="sls_copy">that Improve Training Simulations for Students &amp; Instructors</span>
+  </div>
 </div>
-</div>
-
-
 </div><!-- #content-->
 </div><!--.row -->
 </div><!--.container -->
-
-
-<div style="width:100%; text-align:center; background-color:#d6001c; padding:4px 0; color:#fff; font-size: 17px; letter-spacing: 1px; ">ALL SIMLABSOLUTIONS PRODUCTS ARE FOR INSTRUCTIONAL USE ONLY. NOT FOR HUMAN OR ANIMAL INJECTION OR INGESTION.</div>
-
+<div id="sls_red_div">ALL SIMLABSOLUTIONS PRODUCTS ARE FOR INSTRUCTIONAL USE ONLY. NOT FOR HUMAN OR ANIMAL INJECTION OR INGESTION.</div>
 <div class="container woocommerce">
 <div class="row">
-<div id="content" role="main">
-
-<div class="container woocommerce">
-<div class="row"> 
-
-<div class="col-xs-6" id="sidebar" role="navigation">
-
-<?php get_template_part('includes/sidebar'); ?>
+    <div class="col-xs-6" id="sidebar" role="navigation">
+      <?php get_template_part('includes/sidebar'); ?>
     </div>
-
-
-<div id="content" role="main">
-<a href="<?php echo site_url(); ?>/product-category/emergency-rescue/loaded-emergency-packs/">
-<img style="margin-bottom: 12px; margin-top:24px;" src="<?php echo site_url(); ?>/wp-content/imgs/ems/Loaded_EMS_Packs.png" />
-</a>
-
-
-<a style="margin-top:1em;" href="#" class="eModal-2">
- <img src="<?php echo site_url(); ?>/wp-content/imgs/ns-home-imgs/SLS_Home_catalog.gif" alt="Emergency &amp; Rescue Training" /></a>
-
+    <div id="ns_rotate_wrap">
+      <a href="<?php echo site_url(); ?>/product-category/emergency-rescue/loaded-emergency-packs/">
+      <img style="margin-bottom: 14px; margin-top:6px;" src="<?php echo site_url(); ?>/wp-content/imgs/ems/Loaded_EMS_Packs.png" />
+      </a>
+      <div class="row" style="margin-bottom:14px;">
+        <div class="col-md-6">
+        	<a href="<?php echo site_url(); ?>//product-category/carts-storage/loaded-crash-carts/">
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Carts-Square.png"
+        		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Carts-Square-Hover.png';"
+    				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Carts-Square.png';" />
+        	</a>
+        </div>
+        <div class="col-md-6">
+        	<a href="<?php echo site_url(); ?>/product-category/emergency-rescue/loaded-emergency-packs/">
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-Square.png" 
+        		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-hover.png';"
+    				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Loaded-Emergency-Packs-Square.png';" />
+        	</a>
+        </div>
+      </div>
+      <div class="row" style="margin-bottom:14px;">
+        <div class="col-md-4">
+        	<a href="<?php echo site_url(); ?>/product-category/simulated-iv-bags/">
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square.png"  
+        		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square-Hover.png';"
+    				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/IV-Fluids-Square.png';" />
+        	</a>
+        </div>
+        <div class="col-md-4">
+        	<a href="<?php echo site_url(); ?>/product-category/simulated-iv-bags/">
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square.png"  
+        		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square-Hover.png';"
+    				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Blood-Bags-Square.png';" />
+        	</a>
+        </div>
+        <div class="col-md-4">
+        	<a href="<?php echo site_url(); ?>/product-category/supply-kits/">
+        		<img src="<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square.png"  
+        		     onMouseOver="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square-Hover.png';"
+    				 onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/sls/Supply-Kits-Square.png';" />
+        	</a>
+        </div>
+      </div>
+      <a style="margin-top:1em;" href="#" class="eModal-2">
+      <img src="<?php echo site_url(); ?>/wp-content/imgs/ns-home-imgs/SLS_Home_catalog.gif" alt="Emergency &amp; Rescue Training" /></a>
+    </div>
+  </div>
 </div>
-</div>
-</div>
-
