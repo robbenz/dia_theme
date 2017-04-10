afff233f5b01dcfc2f674d0ac308c66359618df7
Rob Benz <benz_rob@yahoo.com>
2017-04-10 07:41:08 -0400

    this header.php has the search i want

--- includes/header.php
+++ includes/header.php
@@ -48,6 +48,18 @@
 			</a>
 		</div>
 	</div>
+
+
+	<form class="navbar-form navbar-left" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<div class="form-group">
+			<input class="form-control" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search..." name="s" id="s">
+			<input type="hidden" name="post_type" value="product" />
+		</div>
+		<button type="submit" id="searchsubmit" value="<?php esc_attr_x('Search', 'bst') ?>" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+	</form>
+
+
+
 		<div id="form-row"class="row">
 			<div id="form-row-c1" class="form-row-child">
 				<?php
@@ -55,95 +67,106 @@
 				if ( function_exists( 'woo_predictive_search_widget' ) ) woo_predictive_search_widget( $ps_echo );
 				?>
 			</div>
-			<div id="mm-right-contact">
 
+
+
+			<div id="mm-right-contact">
 				<?php
 
- 				$benzitems = '<ul id="%1$s" class="%2$s sf-menu sf-js-enabled">%3$s</ul>';
-
+				$benzitems = '<ul id="%1$s" class="%2$s sf-menu sf-js-enabled">%3$s</ul>';
 				echo '<span class="mm-right-contact-1">' . 'CONTACT US: (877) 593-6011' . '</span>';
 				echo '<span class="mm-right-contact-2">' . '   (M-F: 7-6 EST)' . '</span>' . '<br />';
 
-				wp_nav_menu( array( 'theme_location'  => 'header-menu',
-	                    'items_wrap'      => $benzitems,
-	                    'walker'          => new BENZ_Walker_Nav_Menu
-										) );
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'header-menu',
+						'items_wrap'      => $benzitems,
+						'walker'          => new BENZ_Walker_Nav_Menu
+					) );
 
-										if ( is_user_logged_in() ) {
-											wp_nav_menu( array( 'theme_location'  => 'myaccount',
-											                    'items_wrap'      => $benzitems,
-	                                        'walker'          => new BENZ_Walker_Nav_Menu_MYACCOUNT
-																				) );
-										} else {
-											wp_nav_menu( array( 'theme_location'  => 'sign-in-menu',
-    																			'items_wrap'      => $benzitems,
-																					'walker'          => new BENZ_Walker_Nav_Menu_SIGNIN
-																				) );
-																			};
+					if ( is_user_logged_in() ) {
+						wp_nav_menu(
+							array(
+								'theme_location'  => 'myaccount',
+								'items_wrap'      => $benzitems,
+								'walker'          => new BENZ_Walker_Nav_Menu_MYACCOUNT
+							) );
+						} else {
+							wp_nav_menu(
+								array(
+									'theme_location'  => 'sign-in-menu',
+									'items_wrap'      => $benzitems,
+									'walker'          => new BENZ_Walker_Nav_Menu_SIGNIN
+								) );
+							};
 
-																			echo do_shortcode('[WooCommerceWooCartPro]');
+							echo do_shortcode('[WooCommerceWooCartPro]');
 
-																			?>
-</div> <!-- #mm-right-contact -->
-</div> <!-- .container -->
-</nav>
-</div>
-</header> <!-- #masthead -->
+							?>
+						</div> <!-- #mm-right-contact -->
+					</div> <!-- .container -->
+				</nav>
+			</div>
+		</header> <!-- #masthead -->
 
-<nav id="drop-menu" class="">
-	<div class="container">
-    <div class="navbar-header">
+		<nav id="drop-menu" class="">
+			<div class="container">
+				<div class="navbar-header">
 
-			<?php
-$benzmenu  = 'benz-menu';
-wp_nav_menu(array(
-    'theme_location' => 'home',
-    'items_wrap' => $benzitems,
-    'container_class' => $benzmenu
-));
-wp_nav_menu(array(
-    'theme_location'  => 'medical-equipment',
-    'items_wrap'      => $benzitems,
-    'container_class' => $benzmenu . ' ' . $benzmenu . '-equipt',
-    'walker'          => new BENZ_Walker_Nav_Menu_EQP
-));
+					<?php
 
-$ps_echo = true ;
-if ( function_exists( 'woo_predictive_search_widget' ) ) {
-    wp_nav_menu(array(
-        'theme_location'  => 'parts-search',
-        'items_wrap'      => $benzitems,
-        'container_class' => $benzmenu . ' ' . $benzmenu . '-parts',
-        'walker'          => new BENZ_Walker_Nav_Menu_PS
-    ));
-} else {
-    wp_nav_menu(array(
-        'theme_location'  => 'parts-search',
-        'items_wrap'      => $benzitems,
-        'container_class' => $benzmenu . ' ' . $benzmenu . '-parts'
-    ));
-}
+					$benzmenu  = 'benz-menu';
+					wp_nav_menu( array(
+						'theme_location' => 'home',
+						'items_wrap' => $benzitems,
+						'container_class' => $benzmenu
+					));
 
-wp_nav_menu(array(
-    'theme_location'  => 'repirstmp',
-    'items_wrap'      => $benzitems,
-    'container_class' => $benzmenu . ' ' . $benzmenu . '-repairs'
-  //    'walker'        => new BENZ_Walker_Nav_Menu_RP
-));
-wp_nav_menu(array(
-    'theme_location'  => 'manufacturers',
-    'items_wrap'      => $benzitems,
-    'container_class' => $benzmenu . ' ' . $benzmenu . '-manufacturers',
-    'walker'          => new BENZ_Walker_Nav_Menu_MFT
-));
-wp_nav_menu(array(
-    'theme_location'  => 'about-us',
-    'items_wrap'      => $benzitems,
-    'container_class' => $benzmenu . ' ' . $benzmenu . '-about',
-    'walker'          => new BENZ_Walker_Nav_Menu_ABOUT
-));
-?>
+					wp_nav_menu( array(
+						'theme_location'  => 'medical-equipment',
+						'items_wrap'      => $benzitems,
+						'container_class' => $benzmenu . ' ' . $benzmenu . '-equipt',
+						'walker'          => new BENZ_Walker_Nav_Menu_EQP
+					));
 
-		</div><!-- .navbar-header -->
-	</div> <!-- /.container -->
-</nav>
+					$ps_echo = true ;
+					if ( function_exists( 'woo_predictive_search_widget' ) ) {
+						wp_nav_menu( array(
+							'theme_location'  => 'parts-search',
+							'items_wrap'      => $benzitems,
+							'container_class' => $benzmenu . ' ' . $benzmenu . '-parts',
+							'walker'          => new BENZ_Walker_Nav_Menu_PS
+						));
+
+					} else {
+						wp_nav_menu( array(
+							'theme_location'  => 'parts-search',
+							'items_wrap'      => $benzitems,
+							'container_class' => $benzmenu . ' ' . $benzmenu . '-parts'
+						));
+					}
+
+					wp_nav_menu( array(
+						'theme_location'  => 'repirstmp',
+						'items_wrap'      => $benzitems,
+						'container_class' => $benzmenu . ' ' . $benzmenu . '-repairs'
+  				  //  'walker'        => new BENZ_Walker_Nav_Menu_RP
+					));
+
+					wp_nav_menu( array(
+						'theme_location'  => 'manufacturers',
+						'items_wrap'      => $benzitems,
+						'container_class' => $benzmenu . ' ' . $benzmenu . '-manufacturers',
+						'walker'          => new BENZ_Walker_Nav_Menu_MFT
+					));
+
+					wp_nav_menu( array(
+						'theme_location'  => 'about-us',
+						'items_wrap'      => $benzitems,
+						'container_class' => $benzmenu . ' ' . $benzmenu . '-about',
+						'walker'          => new BENZ_Walker_Nav_Menu_ABOUT
+					));
+					?>
+				</div><!-- .navbar-header -->
+			</div> <!-- /.container -->
+		</nav>
