cd25fe610bc0812da2a9d43cc7763958920676d8
Rob Benz <benz_rob@yahoo.com>
2017-04-14 09:02:38 -0400

    before dev on new hospital shit

--- includes/loops/content.php
+++ includes/loops/content.php
@@ -24,7 +24,7 @@ Alternatively, notice that index.php, category.php and single.php have a post_cl
         </header>
         <section>
             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' );  ?></a>
-            <?php//  the_content( __( '&hellip; ' . __('Continue reading', 'bst' ) . ' <i class="glyphicon glyphicon-arrow-right"></i>', 'bst' ) ); ?>
+
         </section>
         <footer>
             <p class="text-muted" style="margin-bottom: 20px;">
