b8b3ff3c3d79c63068d35d6b33cd0f74ad6730b4
Rob Benz <benz_rob@yahoo.com>
2017-06-02 09:19:04 -0400

    ems bundle pic

--- includes/ems.php
+++ includes/ems.php
@@ -1,4 +1,4 @@
-<div style="float: right; height: 880px; margin-right: -26px; margin-top: 19px; width: 850px;">
+<div style="float: right; height: auto; margin-right: -26px; margin-top: 19px; width: 850px;">
 
 <?php if ( function_exists( 'soliloquy' ) ) { soliloquy( '71778' ); } ?>
 <br />
@@ -44,6 +44,10 @@
     onMouseOut="this.src='<?php echo site_url(); ?>/wp-content/imgs/ems/ems_cots_stretchers.png';"/>
   </a>
 </div>
+
+<a href="<?php echo site_url(); ?>/product-category/emergency-rescue/loaded-emergency-packs/" >
+  <img style="margin-bottom:1em;"src="<?php echo site_url(); ?>/wp-content/imgs/ems/loaded_ems_packs_banner.png" />
+</a>
 
 <a style="margin-top:1em;" href="#" class="eModal-2">
  <img src="<?php echo site_url(); ?>/wp-content/imgs/ems/ems_catalog_button.gif" alt="Emergency &amp; Rescue Training" /></a>
