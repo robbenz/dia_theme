cb127c4c90cbabbadeef9f833960b2ba1e75b6ad
Rob Benz <benz_rob@yahoo.com>
2017-10-27 15:46:49 -0400

    workflow end of day

---
+++ includes/terms-conditions.php
@@ -0,0 +1,61 @@
+<?php
+/* Template Name: Terms & Conditions */
+get_template_part('includes/header');
+
+?>
+  <div class="container" style="background-color: #f6f6f6; margin-top: 1em; border-radius: 0.5em; padding: 1em 2em 2em 2em; margin-bottom: 1em;">
+    <div class="row">
+      <h2 style="font-family: 'ristretto_promedium',Arial,sans-serif; color: #00426a; letter-spacing: 1.1px; font-size: 41px; text-transform: uppercase;">Terms &amp; Conditions</h2>
+      <div class="main-contact-wrap col-12 col-sm-6 col-md-8">
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Accepted Payment Methods<img style="width: 120px; margin: 5px 16px 9px;" src="https://medmattress.com/wp-content/imgs/accepted_cards.png" /></h3>
+        <span style="font-weight: 400;">DiaMedicalUSA.com accepts payments by Purchase Order, Visa, or Mastercard online. Payments must be made in U.S. currency.</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Prices and Price Changes</h3>
+        <span style="font-weight: 400;">Some prices listed do not include freight or handling fees and are subject to correction or change without notice. In the case of substantial increases, customers will be notified on an as needed basis. Orders will be not be processed unless approval is given by the customer. Every customer is given 24 hours to respond to a price increase, if no response is received, DiaMedicalUSA.com will process the order with the corrected pricing. However, we always try to keep our prices as low as possible.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Tax</h3>
+        <span style="font-weight: 400;">DiaMedicalUSA.com does not collect sales tax on any of its products. The purchaser is responsible for remitting any sales tax owed.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Low Price Policy</h3>
+        <span style="font-weight: 400;">It is always our policy to provide the lowest possible price to our customers. Additional savings may apply for bulk purchases.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">International Orders</h3>
+        <span style="font-weight: 400;">DiaMedicalUSA.com can ship internationally. Canadian orders will be shipped UPS Standard unless otherwise specified with additional freight charges added. All other international orders will be shipped by DHL or USPS unless otherwise specified. Please note that orders shipping through the USPS are only traceable through the United States. All international orders will include increased freight charges.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Use of Products</h3>
+        <span style="font-weight: 400;">Purchaser further agrees that all products will be used for their intended purposes only in a safe and legal manner consistent with all applicable state and federal laws and Purchaser’s own internal health and safety policies.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Limitation of Liability</h3>
+        <span style="font-weight: 400;">DiaMedicalUSA.com and DiaMedical USA shall not be liable for any special or consequential damages that result from the use of, or the inability to use, the materials on this site or the performance of the products, even if DiaMedicalUSA.com and DiaMedical USA has been advised of the possibility of such damages. Applicable law may not allow the limitation of exclusion of liability or incidental or consequential damages, so the above limitation or exclusion may not apply to you.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Indemnification</h3>
+        <span style="font-weight: 400;">You agree to indemnify, defend, and hold harmless DiaMedicalUSA.com and DiaMedical USA, its officers, directors, employees, agents, licensors and suppliers (collectively the "Service Providers") from and against all losses, expenses, damages and costs, including reasonable attorneys' fees, resulting from any violation of these terms and conditions or any activity related to your account (including negligent or wrongful conduct) by you or any other person accessing the site using your Internet account.
+</span>
+        <h3 style="font-family: 'ristretto_prolight',Arial,sans-serif; color: #288dcc; font-size: 35px;">Product Specifications</h3>
+        <span style="font-weight: 400;">DiaMedicalUSA.com reserves the right to change product specifications on all items without notice. All efforts are made to display accurate product information, however, due to uncontrollable manufacturing conditions, product specifications and colors are subject to change without notice. Actual product color may differ from online product color.</span>
+
+      </div>
+      <div class="col-6 col-sm-6 col-md-4">
+        <div id="dat_wrap">
+          <h3 style="text-align: center; line-height: 15px; font-size: 49px;">How To Order</h3>
+          <h4 style="text-align: center; margin-bottom: 18px;">from DiaMedical USA</h4>
+          <div id="dat_order_left"><img style="padding-bottom: 32px !important;" src="<?php echo site_url(); ?>wp-content/themes/diamedical/imgs/icon/phone-blue.png" />
+            <img src="<?php echo site_url(); ?>wp-content/themes/diamedical/imgs/icon/fax-blues.png" />
+            <img src="<?php echo site_url(); ?>wp-content/themes/diamedical/imgs/icon/laptop-blues.png" />
+            <img src="<?php echo site_url(); ?>wp-content/themes/diamedical/imgs/icon/mail-blue.png" /></div>
+          <div id="dat_order_rght">
+
+            <em>By Phone:</em>
+            <p class="lt_blue">(877) 593-6011</p>
+            <em>By Fax:</em>
+            <p class="lt_blue">(248) 671-1550</p>
+            <em>By Email:</em>
+            <p class="lt_blue">Orders@DiaMedicalUSA.com</p>
+            <em>By Mail: </em>
+            <p class="lt_blue">7013 Orchard Lake Rd, Suite #110 West Bloomfield, MI 48322</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php get_template_part('includes/footer'); ?>
