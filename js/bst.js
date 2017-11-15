52c6cb829fad85edc49018710508a7946ff477f7
Rob Benz <benz_rob@yahoo.com>
2017-11-15 12:04:15 -0500

    yeah whtever fuck you in britich '

--- js/bst.js
+++ js/bst.js
@@ -112,7 +112,8 @@
       /*** add columns class to long lists in product tabs  ***/
     //  $('.woocommerce-tabs ul').filter(function () {
       $('#woocommerce_tabs ul').filter(function () {
-        return $(this).find('li').length >= 17
+        //return $(this).find('li').length >= 17
+        return $(this).children().length >= 17
       }).addClass('columns_list');
       /*** END ***/
 
