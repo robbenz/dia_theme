51e61f7ab7a8bd1aee3fe13656f2fe95cb85c16e
DiaMedical USA User <dmuser@Michaels-iMac.local>
2017-11-16 09:10:51 -0500

    refactor menu.js

--- js/menu.js
+++ js/menu.js
@@ -8,98 +8,27 @@
     $(".top-menu-main-a").mouseenter(function(){$(this).addClass('hoverline');}).mouseleave(function(){$(this).removeClass('hoverline');});
     $("#masthead").mouseenter(function(){$('.drop-menu-panel').stop().fadeOut(100);});
     $(".drop-menu-panel").mouseleave(function(){$('.drop-menu-panel').stop().fadeOut(100);});
+    $("#main_home").mouseenter(function(){$('.drop-menu-panel').stop().fadeOut(100);});
 
-
-    $("#main_home")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-      });
-
-    $("#main_medical_equipment")
-      .mouseenter(function(){
+    function showTopDropMenus (hoveredLink, shownMenu) {
+      $(hoveredLink).mouseenter(function(){
         $('.drop-menu-panel').stop().fadeOut(100);
         clearTimeout(timer);
         timer = setTimeout(function(){
-          $('#med_equipment_drop').stop().fadeIn(100);
+          $(shownMenu).stop().fadeIn(100);
         }, 200);
-        console.log(timer);
       })
       .mouseleave(function(){
         clearTimeout(timer);
-        console.log(timer);
       });
+    }
 
-    $("#main_part_search")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#parts_drop').stop().fadeIn(100);
-        }, 200);
-        console.log(timer);
-      })
-      .mouseleave(function(){
-        clearTimeout(timer);
-        console.log(timer);
-      });
-
-    $("#main_service")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#service_drop').stop().fadeIn(100);
-        }, 200);
-        console.log(timer);
-      })
-      .mouseleave(function(){
-        clearTimeout(timer);
-        console.log(timer);
-      });
-
-      $("#main_manufacturers")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#mfrs_drop').stop().fadeIn(100);
-        }, 200);
-        console.log(timer);
-      })
-      .mouseleave(function(){
-        clearTimeout(timer);
-        console.log(timer);
-      });
-
-      $("#main_contact_us")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#contact_drop').stop().fadeIn(100);
-        }, 200);
-        console.log(timer);
-      })
-      .mouseleave(function(){
-        clearTimeout(timer);
-        console.log(timer);
-      });
-
-      $("#main_my_account")
-      .mouseenter(function(){
-        $('.drop-menu-panel').stop().fadeOut(100);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-          $('#account_drop').stop().fadeIn(100);
-        }, 200);
-        console.log(timer);
-      })
-      .mouseleave(function(){
-        clearTimeout(timer);
-        console.log(timer);
-      });
-
-
+    showTopDropMenus('#main_medical_equipment', '#med_equipment_drop');
+    showTopDropMenus('#main_part_search', '#parts_drop');
+    showTopDropMenus('#main_service', '#service_drop');
+    showTopDropMenus('#main_manufacturers', '#mfrs_drop');
+    showTopDropMenus('#main_contact_us', '#contact_drop');
+    showTopDropMenus('#main_my_account', '#account_drop');
 
   });
 }(jQuery));
