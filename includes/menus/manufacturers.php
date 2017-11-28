a0fb62644f23a09e8a2fb7136c7c62fdf92b3640
Rob Benz <benz_rob@yahoo.com>
2017-11-28 14:22:29 -0500

    staging push for mft menu

--- includes/menus/manufacturers.php
+++ includes/menus/manufacturers.php
@@ -1,25 +1,32 @@
 <?php
-  $all_mfrs = array("BAXTER","ALARIS","HILL-ROM","INVACARE","HOSPIRA","MEDLINE","MIDMARK","SPAN AMERICA","SIMLABSOLUTIONS","STRYKER","ZOLL","DRIVE","HAUSTED","JOERNS","LUMEX","AIRPAL","GRAHAM-FIELD","AMICO","KCI","FERNO","TENTE","BURKE","3AM","FOLLETT","SUMMIT INDUSTRIES","MEDMIZER","GRAINGER","HARLOFF","HAUSMANN","WELCH ALLYN","STERIS","LAERDAL","LEADING EDGE","AVKIN");
+  $all_mfrs = array("Baxter","Alaris","Hill-rom","Cardionics","Clinton","Invacare","Hospira","Medline","Midmark","Span America","Simlabsolutions","Stryker","Zoll","Drive","Hausted","Joerns","Lumex","Airpal","Graham-field","Amico","Kci","Ferno","Tente","Burke","3am","Follett","Summit Industries","Medmizer","Grainger","Harloff","Hausmann","Welch Allyn","Steris","Laerdal","Leading Edge","Avkin");
   asort($all_mfrs);
   $all_mfrs  = partition($all_mfrs, 4);
 ?>
 
-<nav id="mfrs_drop" class="drop-menu-panel">
+<nav id="mfrs_drop" style="display:block;" class="drop-menu-panel">
   <div class="container">
     <div class="row">
+      <div style="text-align:center;" class="col-sm-4">
+        <!-- <p style="color:#00426a; font-family: 'ristretto_proregular',sans-serif; font-size:34px;">DiaMedical USA</p> -->
+        <p style="color:#00426a; font-family: 'ristretto_proregular',sans-serif; font-size:34px;">Manufacturer Directory</p>
+        <!-- <p style="color:#00426a;font-family: 'Lato', sans-serif; font-size:16px;">Manufacturer Directory</p> -->
+        <img style="width:75%;" src="<?php echo site_url(); ?>/wp-content/imgs/Manufacturers_directory.png" />
+        <p style="font-family: 'Lato', sans-serif; font-size:24px; color:#00426a;margin-top:10px;" ><em><strong>We Can Do That!</strong></em></p>
+      </div>
       <?php foreach ( $all_mfrs as $mfr_list ) : ?>
-        <div class="col-sm-3">
+        <div class="col-sm-2">
           <ul>
             <?php foreach ($mfr_list as $mfr) : ?>
-              <?php  if ($mfr == "HILL-ROM" ) :?>
+              <?php if ($mfr == "Hill-Rom") : ?>
                 <a href="<?php echo site_url(); ?>/product-category/hill-rom-parts-online/">
                   <li class="top-menu-mfr"><strong><?php echo $mfr ; ?></strong></li>
                 </a>
-              <?php elseif ( $mfr == "SIMLABSOLUTIONS" ) : ?>
+              <?php elseif ( $mfr == "Simlabsolutions" ) : ?>
                 <a href="<?php echo site_url(); ?>/simlabsolutions/">
                   <li class="top-menu-mfr"><strong><?php echo $mfr ; ?></strong></li>
                 </a>
-              <?php elseif ( $mfr == "DRIVE" ) : ?>
+              <?php elseif ( $mfr == "Drive" ) : ?>
                 <a href="<?php echo site_url(); ?>/?s=DRIVE+MEDICAL+PARTS&amp;post_type=product">
                   <li class="top-menu-mfr"><strong><?php echo $mfr ; ?></strong></li>
                 </a>
@@ -32,41 +39,10 @@
           </ul>
         </div>
       <?php endforeach; ?>
+
     </div>
-    <!-- <div style="margin-top:8px;text-align:center;" class="row">
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/?s=amico&amp;post_type=product">
-          <img style="width: 120px; margin-top: -21px;"src="<?php echo site_url(); ?>/wp-content/imgs/amico_logo.png" />
-        </a>
-      </div>
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/product-category/hill-rom-parts-online/">
-          <img src="<?php echo site_url(); ?>/wp-content/imgs/hill-rom-logo.png" />
-        </a>
-      </div>
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/?s=STRYKER&amp;post_type=product">
-          <img src="<?php echo site_url(); ?>/wp-content/imgs/strykerlogo.png" />
-        </a>
-      </div>
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/?s=hausted&amp;post_type=product">
-          <img style="margin-top: 16px;" src="<?php echo site_url(); ?>/wp-content/imgs/haustedlogo.png" />
-        </a>
-      </div>
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/?s=midmark&amp;post_type=product">
-          <img src="<?php echo site_url(); ?>/wp-content/imgs/midmarklogo.png" />
-        </a>
-      </div>
-      <div class="col-sm-2">
-        <a href="<?php echo site_url(); ?>/?s=zoll&amp;post_type=product">
-          <img style="width: 135px;" src="<?php echo site_url(); ?>/wp-content/uploads/2017/10/ZOLL-Logo-1.jpg" />
-        </a>
-      </div>
-    </div> -->
   </div>
   <div class="text-center">
-    <p style="margin: 20px 0 0; background-color: #ffad00; padding: 12px;">Don't want to browse manufacturers? Try searching your manufacturer at the top of the page!</p>
+    <p style="margin: 20px 0 0; background-color: #78be20; padding: 12px; color:#fff; font-weight:600">We work with over 500 manufacturers for your one stop shopping convenience.</p>
   </div>
 </nav>
--- includes/menus/submenus/ltc-menu.php
+++ includes/menus/submenus/ltc-menu.php
@@ -2,8 +2,8 @@
   <div class="row">
     <div class="col-md-12">
       <a href="<?php echo site_url(); ?>/long-term-care/">
-      <h3 style="background-color:#72d0f4;">Long Term Care Equipment</h3>
-    </a>
+        <h3 style="background-color:#72d0f4;">Long Term Care Equipment</h3>
+      </a>
     </div>
   </div>
   <div class="row">
@@ -12,7 +12,7 @@
         <?php
     diaLink('cat', 'h4', 'bariatric-care');
     diaLink('cat', 'li', 'bariatric-bath-safety');
-    diaLink('cat', 'li', 'bariatric-hospital-beds-stretchers/');
+    diaLink('cat', 'li', 'bariatric-hospital-beds-stretchers');
     diaLink('cat', 'li', 'bariatric-mobility');
     diaLink('cat', 'li', 'bariatric-patient-handling');
     diaLink('cat', 'li', 'bariatric-speciality-seating');
@@ -30,93 +30,43 @@
     </div>
     <div class="col-md-4">
       <ul>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/">
-          <h4>Medical Supplies</h4>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/adhesives/">
-          <li>Adhesives</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/gloves-supplies-2/">
-          <li>Gloves</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/ostomy/">
-          <li>Ostomy</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/urinary-catheter-supplies/">
-          <li>Urinary Catheter Supplies</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/supplies-2/">
-          <li class="menu-view-all">View All</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/">
-          <h4>Mobility</h4></a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/canes/">
-          <li>Canes</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/crutches/">
-          <li>Crutches</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/power-scooters/">
-          <li>Power Scooters</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/rollators/">
-          <li>Rollators</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/wheelchairs-mobility/">
-          <li>Wheelchairs</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/mobility/">
-          <li class="menu-view-all">View All</li>
-        </a>
+        <?php
+    diaLink('cat', 'h4', 'supplies-2');
+    diaLink('cat', 'li', 'adhesives');
+    diaLink('cat', 'li', 'gloves-supplies-2');
+    diaLink('cat', 'li', 'ostomy');
+    diaLink('cat', 'li', 'urinary-catheter-supplies');
+    diaLink('cat', 'li', 'supplies-2', 'view-all');
+
+    diaLink('cat', 'h4', 'mobility');
+    diaLink('cat', 'li', 'canes');
+    diaLink('cat', 'li', 'crutches');
+    diaLink('cat', 'li', 'power-scooters');
+    diaLink('cat', 'li', 'rollators');
+    diaLink('cat', 'li', 'wheelchairs-mobility');
+    diaLink('cat', 'li', 'mobility', 'view-all');
+    ?>
       </ul>
     </div>
     <div class="col-md-4">
       <ul>
-        <a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/">
-          <h4>Infusion &amp; Dialysis</h4>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/infusion-pumps/">
-          <li>Infusion Pumps</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/iv-poles/">
-          <li>IV Poles</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/iv-tubing/">
-          <li>IV Tubing</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/infusion-dialysis/">
-          <li class="menu-view-all">View All</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/">
-          <h4>Patient Care</h4>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/apparel/">
-          <li>Apparel</li>
-        </a>
-        <a  href="<?php echo site_url(); ?>/product-category/patient-care/bedside-care-nursing-supplies-patient-care/">
-          <li>Bedside Care</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/incontinence/">
-          <li>Incontinence</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/patient-lifts/">
-          <li>Patient Lifts</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/personal-care/">
-          <li>Personal Care</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/positioning-protectors/">
-          <li>Positioning &amp; Protectors</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/showerbath/">
-          <li>Shower &amp; Bath</li>
-        </a>
-        <a  href="<?php echo site_url(); ?>/product-category/patient-care/transfer-equipment/">
-          <li>Transfer Equipment</li>
-        </a>
-        <a href="<?php echo site_url(); ?>/product-category/patient-care/">
-          <li class="menu-view-all">View All</li>
-        </a>
+        <?php
+    diaLink('cat', 'h4', 'infusion-dialysis');
+    diaLink('cat', 'li', 'infusion-pumps');
+    diaLink('cat', 'li', 'iv-tubing');
+    diaLink('cat', 'li', 'infusion-dialysis', 'view-all');
+
+    diaLink('cat', 'h4', 'patient-care' );
+    diaLink('cat', 'li', 'apparel');
+    diaLink('cat', 'li', 'bedside-care-nursing-supplies-patient-care');
+    diaLink('cat', 'li', 'incontinence');
+    diaLink('cat', 'li', 'patient-lifts');
+    diaLink('cat', 'li', 'personal-care');
+    diaLink('cat', 'li', 'positioning-protectors');
+    diaLink('cat', 'li', 'showerbath');
+    diaLink('cat', 'li', 'transfer-equipment');
+    diaLink('cat', 'li', 'patient-care', 'view-all' );
+        ?>
       </ul>
     </div>
   </div>
