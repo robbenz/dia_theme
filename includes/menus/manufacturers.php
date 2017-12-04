a0d581fc69d8137156c98176a2e9dc995f6a2247
DiaMedical USA User <rbenz@diamedicalusa.com>
2017-12-04 12:39:16 -0500

    staging push trial /

--- includes/menus/manufacturers.php
+++ includes/menus/manufacturers.php
@@ -4,7 +4,7 @@
   $all_mfrs  = partition($all_mfrs, 4);
 ?>
 
-  <nav id="mfrs_drop" style="display:block;" class="drop-menu-panel">
+  <nav id="mfrs_drop" class="drop-menu-panel">
     <div class="container">
       <div style="text-align:center" class="row">
         <div class="col-md-12">
