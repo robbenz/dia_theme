396c395450d7ad85e5fd7e3a37bb4a7ab2fac0d1
DiaMedical USA User <rbenz@diamedicalusa.com>
2018-02-06 11:19:09 -0500

    email footer

--- woocommerce/emails/email-footer.php
+++ woocommerce/emails/email-footer.php
@@ -34,15 +34,32 @@ if ( ! defined( 'ABSPATH' ) ) {
                         	<tr>
                             	<td align="center" valign="top">
                                     <!-- Footer -->
-                                	<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+                                	<table border="0" cellpadding="10" cellspacing="0" width="500" id="template_footer">
                                     	<tr>
                                         	<td valign="top">
                                                 <table border="0" cellpadding="10" cellspacing="0" width="100%">
-                                                    <tr>
-                                                        <td colspan="2" valign="middle" id="credit">
-                                                        	<?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) ) ) ) ); ?>
-                                                        </td>
-                                                    </tr>
+																									<tr>
+																                    <td class="content-block">
+																                      <a href="https://medmattress.com">
+																                        <img style="float:left;width:240px;" src="https://diamedicalusa.com//wp-content/imgs/emails/medmattresslogo.png" />
+																                      </a>
+																                      <a href="https://diamedicalusa.com/simlabsolutions">
+																                        <img style="float:right;width:235px;" src="https://diamedicalusa.com/wp-content/imgs/includes/sim-lab.png" />
+																                      </a>
+																                    </td>
+																                  </tr>
+																                  <tr>
+																                    <td style="text-align:center;line-height:28px;" class="content-block">7013 Orchard Lake Rd, Suite #110, West Bloomfield, MI 48322</td>
+																                    <!-- .content-block -->
+																                  </tr>
+																                  <tr>
+																                    <td style="text-align:center;line-height:28px;" class="content-block">P. 877-593-6011 | F. 248-671-1550 | E. info@diamedicalusa.com</td>
+																                    <!-- .content-block -->
+																                  </tr>
+																                  <tr>
+																                    <td style="text-align:center;line-height:28px;" class="content-block">www.DiaMedicalUSA.com | www.MedMattress.com | www.SimLabSolutions.com</td>
+																                    <!-- .content-block -->
+																                  </tr>
                                                 </table>
                                             </td>
                                         </tr>
