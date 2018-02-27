5218afb1c24d56e41e51bb876bbbe1759a0d7e5c
DiaMedical USA User <benz_rob@yahoo.com>
2018-02-27 07:31:04 -0500

    functions change

--- functions.php
+++ functions.php
@@ -1413,12 +1413,12 @@ function diaLink($cat, $html, $slug, $view = 'menu-view-all'){
 
 // update some shit
 //
-// $update_stuff = array (
-//
-// );
+$update_stuff = array (
 
-// foreach ($update_stuff as $thing) {
-//   //update_post_meta( $thing, 'dia_whitespace_adj', 'yes' );
-//   //update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2017/09/simusuit-logo.jpg' );
-//   update_post_meta( $thing, 'dia_admin_quote', 'no' );
-// }
+);
+
+foreach ($update_stuff as $thing) {
+  //update_post_meta( $thing, 'dia_whitespace_adj', 'yes' );
+  //update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2017/09/simusuit-logo.jpg' );
+  update_post_meta( $thing, 'dia_admin_quote', 'no' );
+}
