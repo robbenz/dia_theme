8180e9d993b1b83fcc0dd239254ac53a405904d7
DiaMedical USA User <benz_rob@yahoo.com>
2018-03-09 12:46:28 -0500

    changes to related products to show product slider if there are no other products in that category

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -30,7 +30,11 @@ if ( empty( $product ) || ! $product->exists() ) {
 
 $related = $product->get_related( $posts_per_page );
 
-if ( sizeof( $related ) === 0 ) return;
+if ( sizeof( $related ) === 0 ) {
+	echo'<h2 style="margin-top: 0px;text-transform: uppercase;color: #78be20;font-size: 20px;font-weight: 400;">No Related Products Found. Browse Most Popular Products Below.</h2>';
+	echo do_shortcode('[wpb-product-slider product_type="featured"]');
+	return;
+}
 
 $args = apply_filters( 'woocommerce_related_products_args', array(
 	'post_type'            => 'product',
