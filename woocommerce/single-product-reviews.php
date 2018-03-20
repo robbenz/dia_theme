54d1e947d27b3391b81fab228bdc59e672c06fcc
DiaMedical USA User <benz_rob@yahoo.com>
2018-03-20 09:29:40 -0400

    commit before merge

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -100,7 +100,6 @@ if ( ! comments_open() ) {
 					$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . __( 'Your Review', 'woocommerce' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
 
 					comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
-					// _pre($comment_form);
 				?>
 			</div>
 		</div>
