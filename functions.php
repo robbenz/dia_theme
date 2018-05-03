fe11fc8e39ba6ef87989097a0ae3f0ec39c1639c
DiaMedical USA User <benz_rob@yahoo.com>
2018-05-03 10:19:48 -0400

    and un comment

--- functions.php
+++ functions.php
@@ -1859,18 +1859,18 @@ function dia_add_quote_created_sorting( $column, $post_id ) {
 
 // update some shit
 //
-$update_stuff = array (
-  41579,
-  41590,
-  41591,
-  90800,
-  90801,
-  90802
-);
-
-foreach ($update_stuff as $thing) {
-  //update_post_meta( $thing, 'dia_whitespace_adj', 'yes' );
-  update_post_meta( $thing, '_thumbnail_id', '97038' );
-  //update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2017/09/simusuit-logo.jpg' );
-  // update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2018/05/Medline.jpg' );
-}
+// $update_stuff = array (
+//   41579,
+//   41590,
+//   41591,
+//   90800,
+//   90801,
+//   90802
+// );
+//
+// foreach ($update_stuff as $thing) {
+//   //update_post_meta( $thing, 'dia_whitespace_adj', 'yes' );
+//   update_post_meta( $thing, '_thumbnail_id', '97038' );
+//   //update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2017/09/simusuit-logo.jpg' );
+//   // update_post_meta( $thing, 'mft_image', 'http://diamedicalusa.com/wp-content/uploads/2018/05/Medline.jpg' );
+// }
